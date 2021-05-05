afcbc44a2c9de485d2665e70fe1673e513297c44
andreysey <andreyseynew@gmail.com>
2021-05-05 15:04:58 +0300

    in slider.js add slideIn block

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -17,6 +17,20 @@ export default class Slider {
             this.slideIndex = this.slides.length;
         }
 
+        try {
+            this.hanson.style.opacity = '0';
+
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.classList.add('slideInUp');
+                    this.hanson.style.opacity = '1';
+                },3000);
+            } else {
+                this.hanson.classList.remove('slideInUp')
+            }
+        } catch (e) {}
+
         this.slides.forEach(slide => {
             slide.style.display = 'none';
         });
@@ -29,6 +43,10 @@ export default class Slider {
     }
 
     render() {
+        try {
+            this.hanson = document.querySelector('.hanson');
+        } catch (e) {}
+
         this.btns.forEach(btn => {
             btn.addEventListener('click', () => {
                 this.plusSlides(1);
