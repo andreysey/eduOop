5b7628dfd39c96d6bd5123446643b7826a7ebbf3
andreysey <andreyseynew@gmail.com>
2021-05-05 21:43:57 +0300

    add class VideoPlayer.js use youtube api

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import Slider from "./modules/slider";
+import VideoPlayer from "./modules/VideoPlayer";
 
 window.addEventListener('DOMContentLoaded', () => {
 
    const slider = new Slider('.page', '.next');
    slider.render();
+
+   const videoPlayer = new VideoPlayer('.showup .play', '.overlay');
+   videoPlayer.init();
 });
