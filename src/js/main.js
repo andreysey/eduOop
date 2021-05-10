5079d7806a2d98eec9b5c04121837bbde53e4d89
andreysey <andreyseynew@gmail.com>
2021-05-10 18:31:19 +0300

    add more slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,42 @@
-import Slider from "./modules/slider";
-import VideoPlayer from "./modules/VideoPlayer";
+import VideoPlayer from "./modules/videoPlayer";
+import MainSlider from "./modules/slider/slider-main";
+import MiniSlider from "./modules/slider/slider-mini";
 
 window.addEventListener('DOMContentLoaded', () => {
+    const mainSliderArg = {container: '.page', btns: '.next'}
 
-   const slider = new Slider('.page', '.next');
-   slider.render();
+    const mainSlider = new MainSlider(mainSliderArg);
+    mainSlider.render();
 
-   const videoPlayer = new VideoPlayer('.showup .play', '.overlay');
-   videoPlayer.init();
+    const showUpSlider = new MiniSlider({
+        container: '.showup__content-slider',
+        next: '.showup__next',
+        prev: '.showup__prev',
+        activeClass: 'card-active', // non dot in class
+        animate: true
+    });
+    showUpSlider.init();
+
+    const modulesSlider = new MiniSlider({
+        container: '.modules__content-slider',
+        next: '.modules__info-btns .slick-next',
+        prev: '.modules__info-btns .slick-prev',
+        activeClass: 'card-active', // non dot in class
+        animate: true,
+        autoplay: true
+    });
+    modulesSlider.init();
+
+    const feedSlider = new MiniSlider({
+        container: '.feed__slider',
+        next: '.feed__slider .slick-next',
+        prev: '.feed__slider .slick-prev',
+        activeClass: 'feed__item-active', // non dot in class
+    });
+    feedSlider.init();
+
+
+
+    const videoPlayer = new VideoPlayer('.showup .play', '.overlay');
+    videoPlayer.init();
 });
